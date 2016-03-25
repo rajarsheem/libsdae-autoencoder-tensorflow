73cb8b38b58df8044757482b597a162618cb6612
Nilabhra <nilabhraroy@ymail.com>
2016-03-25 23:32:02 +0530

    Formatted all the code to pep-8 standard

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ from stacked_autoencoder import StackedAutoencoder
 iris = datasets.load_iris()
 x = iris.data
 
-result = StackedAutoencoder(x, dims=[5, 4, 3], noise='gaussian', epoch=1000).encode()
+result = StackedAutoencoder(
+    x, dims=[5, 4, 3], noise='gaussian', epoch=1000
+    ).encode()
 print(result)
 print(result.shape)
