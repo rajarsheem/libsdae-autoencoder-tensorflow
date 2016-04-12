ec8a49a4d300ba8e0f46f7b614944d4024924a0b
rajarsheem <rajarsheem@gmail.com>
2016-04-12 13:20:25 +0530

    added test.ipynb

--- test.py
+++ test.py
@@ -3,13 +3,15 @@
 
 from sklearn import datasets
 import numpy as np
+
 from deepautoencoder import StackedAutoEncoder
 
 iris = datasets.load_iris()
 x = iris.data
 model = StackedAutoEncoder(dims=[5, 6], activations=['sigmoid', 'tanh'], noise='gaussian', epoch=200,
-                           loss='rmse', lr=0.005)
+                          loss='rmse', lr=0.005)
 result = model.fit_transform(x)
 # model.fit(x)
 # result = model.transform(np.random.rand(5, x.shape[1]))
 print(result.shape)
+
