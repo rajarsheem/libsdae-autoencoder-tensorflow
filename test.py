06d8aa071613223b4948f295cd5ba298198669e6
rajarsheem <rajarsheem@gmail.com>
2016-12-06 17:54:20 +0530

    add demo for mnist data

--- test.py
+++ test.py
@@ -1,17 +1,16 @@
-from sklearn import datasets
 from deepautoencoder import StackedAutoEncoder
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
-iris = datasets.load_iris().data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+data, target = mnist.train.images, mnist.train.labels
 
-model = StackedAutoEncoder(
-    dims=[5, 5],
-    activations=['linear', 'linear'],
-    epoch=[1000, 1000],
-    loss='rmse',
-    lr=0.007,
-    batch_size=50,
-    print_step=200
-)
+# train / test  split
+idx = np.random.rand(data.shape[0]) < 0.8
+train_X, train_Y = data[idx], target[idx]
+test_X, test_Y = data[~idx], target[~idx]
 
-a = model.fit_transform(iris)
-print(a)
+model = StackedAutoEncoder(dims=[200, 200], activations=['linear', 'linear'], epoch=[
+                           3000, 3000], loss='rmse', lr=0.007, batch_size=100, print_step=200)
+model.fit(train_X)
+test_X_ = model.transform(test_X)
