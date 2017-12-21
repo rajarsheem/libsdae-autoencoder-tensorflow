8a68c8dc72d927415df34b9bdb40610cfb9bf295
Feserich <Feserich@users.noreply.github.com>
2017-12-21 22:51:12 +0100

    Upadated test.py with some examples

--- test.py
+++ test.py
@@ -8,9 +8,16 @@ data, target = mnist.train.images, mnist.train.labels
 # train / test  split
 idx = np.random.rand(data.shape[0]) < 0.8
 train_X, train_Y = data[idx], target[idx]
-test_X, test_Y = data[~idx], target[~idx]
+val_X, val_Y = data[~idx], target[~idx]
 
 model = StackedAutoEncoder(dims=[200, 200], activations=['linear', 'linear'], epoch=[
                            3000, 3000], loss='rmse', lr=0.007, batch_size=100, print_step=200)
-model.fit(train_X, test_X)
-test_X_latent = model.transform(test_X)
+model.fit(train_X, val_X)
+
+# Alternative: without global validation loss during training
+#model.fit(train_X)
+
+val_X_latent = model.transform(val_X)
+
+# Fit and train model with one method
+#train_X_latent = model.fit_transform(train_X)
