7e9d19fd6f8f85dde18c92f51c008cf94730b5f3
cooper@manjaroPC <fabian.fesenmayr@gmail.com>
2017-12-22 13:18:38 +0100

    added Metric: R¹-Score

--- deepautoencoder/stacked_autoencoder.py
+++ deepautoencoder/stacked_autoencoder.py
@@ -1,6 +1,7 @@
 import numpy as np
 import deepautoencoder.utils as utils
 import tensorflow as tf
+from sklearn.metrics import r2_score
 
 allowed_activations = ['sigmoid', 'tanh', 'softmax', 'relu', 'linear']
 allowed_noises = [None, 'gaussian', 'mask']
@@ -119,12 +120,18 @@ class StackedAutoEncoder:
             sess.run(train_op, feed_dict={x: b_x, x_: b_x_})
             if (i + 1) % print_step == 0:
                 loss_train = sess.run(loss, feed_dict={x: data_x, x_: data_x_})
+                dec_train = sess.run(decoded, feed_dict={x: data_x})
                 if val_x is None:
-                    print('epoch {0}: global train loss = {1}'.format(i, loss_train))
+                    print('epoch {0}: train loss = {1:.5f}, R²-Score Train = {2:.2f}'
+                          .format(i, loss_train, r2_score(data_x_, dec_train)))
                 else:
                     loss_val = sess.run(loss, feed_dict={x: val_x, x_: val_x_})
-                    print('epoch {0}: global train loss = {1}, global validation loss = {2}'
-                          .format(i, loss_train, loss_val))
+                    dec_val = sess.run(decoded, feed_dict={x: val_x})
+                    print('epoch {0}: train loss = {1:.5f}, validation loss = {2:.5f},'
+                          ' R²-Score Train = {3:.2f}, R²-Score Val = {4:.2f}'
+                          .format(i, loss_train, loss_val, r2_score(data_x_, dec_train), r2_score(val_x_, dec_val)))
+
+
 
 
         self.loss_val = loss_train
@@ -133,7 +140,7 @@ class StackedAutoEncoder:
         self.weights.append(sess.run(encode['weights']))
         self.biases.append(sess.run(encode['biases']))
         if val_x is not None:
-            val_x = sess.run(encoded, feed_dict={x: val_x})
+            val_x = sess.run(encoded, feed_dict={x: val_x_})
         return sess.run(encoded, feed_dict={x: data_x_}), val_x
 
     def activate(self, linear, name):
